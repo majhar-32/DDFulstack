b88a700acb9c34fe92ef860ceed88bab11ea1349
majhar-32 <profideainformation@gmail.com>
2025-10-03 02:30:49 +0600

    dockerized fullstack

--- ddfrontend/my-project/src/services/api.js
+++ ddfrontend/my-project/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 // আপনার ব্যাকএন্ড সার্ভারের বেস URL
-const API_URL = 'http://localhost:8080/api/';
+const API_URL = '/api/'; 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
